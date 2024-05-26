e85d02f80c8739ba6bd74490e7ae8cfcddc0b431
Evgesha6 <shirshovazhenia1@gmail.com>
2024-05-26 17:47:07 +0300

    test_header

--- locators/locators_main_page.py
+++ locators/locators_main_page.py
@@ -12,7 +12,7 @@ class MainPage(WebPage):
 
         super().__init__(web_driver, url)
 
-    btn_headers_home = WebElement(ID='menu-item-11,"HOME"')
-    btn_headers_tour = WebElement(ID='menu-item-12,"TOUR"')
-    btn_headers_merch = WebElement(ID='menu-item-13,"MERCH"')
-    btn_headers_music = WebElement(ID='menu-item-14,"MUSIC"')
+    # btn_headers_home = WebElement(ID='//div[@id="menu-item-14"]//href[contains(text(),"Music")]')
+    # btn_headers_tour = WebElement(ID='menu-item-12,"TOUR"')
+    # btn_headers_merch = WebElement(ID='menu-item-13,"MERCH"')
+    btn_headers_music = WebElement(xpath='//div[@id="menu-item-14"]//a[contains(@href,"Music")]')
