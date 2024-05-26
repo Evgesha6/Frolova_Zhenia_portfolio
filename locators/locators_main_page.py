ce7d0a1074982fb701ef3818e8c4645a5b739746
Evgesha6 <shirshovazhenia1@gmail.com>
2024-05-26 20:45:00 +0300

    test_header_footer

--- locators/locators_main_page.py
+++ locators/locators_main_page.py
@@ -12,7 +12,13 @@ class MainPage(WebPage):
 
         super().__init__(web_driver, url)
 
-    # btn_headers_home = WebElement(ID='//div[@id="menu-item-14"]//href[contains(text(),"Music")]')
-    # btn_headers_tour = WebElement(ID='menu-item-12,"TOUR"')
-    # btn_headers_merch = WebElement(ID='menu-item-13,"MERCH"')
-    btn_headers_music = WebElement(xpath='//div[@id="menu-item-14"]//a[contains(@href,"Music")]')
+    btn_headers_home = WebElement(id='menu-item-11')
+    btn_headers_tour = WebElement(id='menu-item-12')
+    btn_headers_merch = WebElement(id='menu-item-13')
+    btn_headers_music = WebElement(id='menu-item-14')
+    # btn_headers_facebook = WebElement(id='menu-item-16')
+
+    btn_footers_home = WebElement(xpath='(//footer//a)[1]')
+    btn_footers_tour = WebElement(xpath='(//footer//a)[2]')
+    btn_footers_merch = WebElement(xpath='(//footer//a)[3]')
+    btn_footers_music = WebElement(xpath='(//footer//a)[4]')
--- test/test_main_page.py
+++ test/test_main_page.py
@@ -11,13 +11,33 @@ def test_headers(web_browser):
     locators = MainPage(web_browser)
 
     header_elements = [
-        # (locators.btn_headers_home, 'HOME'),
-        # (locators.btn_headers_tour, 'TOUR'),
-        # (locators.btn_headers_merch, 'MERCH'),
+        (locators.btn_headers_home, 'HOME'),
+        (locators.btn_headers_tour, 'TOUR'),
+        (locators.btn_headers_merch, 'MERCH'),
         (locators.btn_headers_music, 'MUSIC')
     ]
 
     for elements, elements_text in header_elements:
+        check.is_true(elements.is_visible())
+        check.is_true(elements.is_clickable())
+        check.equal(elements.get_text(), elements_text)
+
+
+@allure.story('Тест для проверки главной страницы')
+@allure.feature('Тест для проверки футера')
+def test_footers(web_browser):
+    """Этот тест проверяет футер главной страницы"""
+
+    locators = MainPage(web_browser)
+
+    footer_elements = [
+        (locators.btn_footers_home, 'HOME'),
+        (locators.btn_footers_tour, 'TOUR'),
+        (locators.btn_footers_merch, 'MERCH'),
+        (locators.btn_footers_music, 'MUSIC')
+    ]
+
+    for elements, elements_text in footer_elements:
         check.is_true(elements.is_visible())
         check.is_true(elements.is_clickable())
         check.equal(elements.get_text(), elements_text)
